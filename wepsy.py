f3e5ddc79961761eaad95abd8066e21168a31a44
Ayush Shah <shahayush001@gmail.com>
2021-08-31 21:34:12 +0530

    test

--- wepsy.py
+++ wepsy.py
@@ -16,7 +16,7 @@ except:
 
 
 search = "#astrophotography OR #tonystark OR TASM OR from:@NASA OR #landscapes OR #sunsets OR -donations -win -tools -btc -fund -funds -donation -stay -cosplay -safe -$LIGHT -staysafe -bitcoin -join -podcast (#lightning) OR #todayinhistory OR #galaxies OR hubble"
-interval = 10
+interval = 1200
 
 for tweet in tweepy.Cursor(api.search, search + "lang:en min_faves:10 min_retweets:5 -filter:replies -bollywood -celebrities -politics -sadhguru -art").items():
 
