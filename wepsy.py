1b57172e975b2c834eee7ef8e1922aba067143af
Ayush Shah <shahayush001@gmail.com>
2021-09-16 00:42:49 +0530

    updated queries and interval

--- wepsy.py
+++ wepsy.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
 import tweepy
 import time
 
+api_key = os.environ.get("api_key")
+api_key_secret = os.environ.get("api_key_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
 
-auth = tweepy.OAuthHandler("nQRWNN72f7XGpEldU12fXPtfo", "zHCaShQWmCmYGlWpLJSrBQphsWSjGwhps2LuHe2Qa7yUDgTWyg")
-auth.set_access_token("1431149377368756225-6fXtB5QB9aeYd57jysHW0pdwFhPtjk", "K5YFP5PafTJWsuEZxxoJru3WSCTY9jPaIkhv86Qe1CCaf")
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
@@ -15,10 +22,10 @@ except:
     print("Error during Authentication")
 
 
-search = "#astrophotography OR from:NASA OR from:AstronomyMag OR from:NewsfromScience OR -donations -win -tools -btc -fund -funds -donation -stay -cosplay -safe -$LIGHT -bitcoin -join -podcast -staysafe -pod #lightning OR from:wired OR from:verge OR from:rajshamani OR from:mashable OR hubble OR #mountains OR HiRISE OR from:MarsMissionImgs OR #Todayinhistory -wion"
-interval = 60 * 10
+search = "#astrophotography OR from:NASA OR from:AstronomyMag OR from:ESA OR from:NatGeoTravel OR from:IndiaHistoryPic OR from:ForbesTech OR from:WIREDScience OR from:ScienceMagazine OR from:NewsfromScience OR from:wired OR from:verge OR from:rajshamani OR from:mashable OR hubble OR HiRISE OR from:MarsMissionImgs OR #Todayinhistory -WIONews OR from:Seeker OR from:archillect OR from:Positive_Call OR from:Inc"
+interval = 60 * 8
 
-for tweet in tweepy.Cursor(api.search, search + "lang:en -filter:replies min_faves:10 min_retweets:2 -bollywood -celebrities -sex -art -porn -dick -pakistan -pilgrimage -ndma -sadhguru -boob").items():
+for tweet in tweepy.Cursor(api.search, search + "lang:en -filter:replies min_faves:10 min_retweets:2").items():
 
     if not tweet.retweeted:
 
